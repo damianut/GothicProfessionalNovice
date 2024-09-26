14680ebc43dbd8d4ad300696433fc6dacc7f2407
damianut <41262420+damianut@users.noreply.github.com>
2024-09-26 20:02:54 +0000

    Initialize patch

---
+++ Ninja/GothicProfessionalNovice/Content/init.d
@@ -0,0 +1,9 @@
+/*
+ * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
+ */
+func void Ninja_GothicProfessionalNovice_Init() {
+    MEM_InitAll();
+
+    // WRITE YOUR INITIALIZATIONS HERE
+
+};
