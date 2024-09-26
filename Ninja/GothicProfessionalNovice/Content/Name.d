c5386a8ee28b9969a9d233d8effa8cff462f30cf
damianut <damianas1999@gmail.com>
2024-09-26 22:28:00 +0200

    Test overwriting

--- Ninja/GothicProfessionalNovice/Content/Name.d
+++ Ninja/GothicProfessionalNovice/Content/Name.d
@@ -1,6 +1,1 @@
-func void Patch_GothicProfessionalNovice_ChangeNoviceName()
-{
-    var int novize; novize = MEM_GetSymbol("NAME_Novize");
-    var zCPar_Symbol novizeSymb; novizeSymb = _^(novize);
-    MEM_WriteString(novizeSymb.content, "Profesjonalista");
-};
+const string NAME_Novize = "Profesjonalista";
--- Ninja/GothicProfessionalNovice/Content/init.d
+++
@@ -1,9 +0,0 @@
-/*
- * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
- */
-func void Ninja_GothicProfessionalNovice_Init() {
-    MEM_InitAll();
-
-    // WRITE YOUR INITIALIZATIONS HERE
-    Patch_GothicProfessionalNovice_ChangeNoviceName();
-};
