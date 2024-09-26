38718010c2f47c46bc80bf04884b54aa1b76a6dd
damianut <damianas1999@gmail.com>
2024-09-26 22:18:57 +0200

    v.1.0.0

---
+++ Ninja/GothicProfessionalNovice/Content/Name.d
@@ -0,0 +1,6 @@
+func void Patch_GNP_ChangeNoviceName()
+{
+    var int novize; novize = MEM_GetSymbol("NAME_Novize");
+    var zCPar_Symbol novizeSymb; novizeSymb = _^(novize);
+    MEM_WriteString(novizeSymb.content, "Profesjonalista");
+};
